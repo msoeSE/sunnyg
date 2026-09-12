d65b2ac99217ba9b995a73386cc8c5bebd46cc7b
Chris Taylor <gitlab@taylor.bz>
2026-09-12 09:26:23 -0500

    Add 2023 Genesis G80 Electrified platform support

--- opendbc_repo/opendbc/car/hyundai/values.py
+++ opendbc_repo/opendbc/car/hyundai/values.py
@@ -620,6 +620,11 @@ class CAR(Platforms):
     CarSpecs(mass=2060, wheelbase=3.01, steerRatio=16.5),
     flags=HyundaiFlags.LEGACY,
   )
+  GENESIS_G80_ELECTRIFIED = HyundaiCanFDPlatformConfig(
+    [HyundaiCarDocs("Genesis G80 Electrified 2023", "All", car_parts=CarParts.common([CarHarness.hyundai_k]))],
+    CarSpecs(mass=2280, wheelbase=3.01, steerRatio=14.0),
+    flags=HyundaiFlags.EV | HyundaiFlags.CANFD_RADAR_SCC,
+  )
   GENESIS_G80_2ND_GEN_FL = HyundaiCanFDPlatformConfig(
     [HyundaiCarDocs("Genesis G80 (2.5T Advanced Trim, with HDA II) 2024", "Highway Driving Assist II", car_parts=CarParts.common([CarHarness.hyundai_p]))],
     CarSpecs(mass=2060, wheelbase=3.00, steerRatio=14.0),
